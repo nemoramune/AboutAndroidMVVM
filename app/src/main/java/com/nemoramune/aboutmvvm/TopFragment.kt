69b662666f3ecc1cfb5abd08d0ab988fa45acf7b
nemoramune <nemoramune@gmail.com>
2022-11-22 20:15:41 +0900

    TopFragmentでGlideを使って画像読み込み
    (cherry picked from commit c8d21fd864d03bdac618ff45a686feb805f811ac)

--- app/src/main/java/com/nemoramune/aboutmvvm/TopFragment.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/TopFragment.kt
@@ -7,4 +7,14 @@ import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.nemoramune.aboutmvvm.databinding.FragmentTopBinding
 
-class TopFragment: Fragment(R.layout.fragment_top)
+class TopFragment: Fragment(R.layout.fragment_top) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentTopBinding.bind(view)
+        val imageView = binding.imageView
+        val imageUrl = "https://foodish-api.herokuapp.com/images/pizza/pizza50.jpg"
+        Glide.with(imageView)
+            .load(imageUrl)
+            .into(imageView)
+    }
+}
