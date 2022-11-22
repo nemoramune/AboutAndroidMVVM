c19b3db32f19d70ce45cdd3b6b1d3df4f91026b2
nemoramune <nemoramune@gmail.com>
2022-11-22 20:09:09 +0900

    TopFragmentを追加

---
+++ app/src/main/java/com/nemoramune/aboutmvvm/TopFragment.kt
@@ -0,0 +1,10 @@
+package com.nemoramune.aboutmvvm
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.nemoramune.aboutmvvm.databinding.FragmentTopBinding
+
+class TopFragment: Fragment(R.layout.fragment_top)
