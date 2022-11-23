a210d3d34042e16025051beb07d920e888e7080a
nemoramune <nemoramune@gmail.com>
2022-11-23 12:18:39 +0900

    TopFragmentでTopViewModelのerrorFlowを購読
    (cherry picked from commit 87b06b11a4cb7aab4a267c9e20a525f2ff9ac712)

--- app/src/main/java/com/nemoramune/aboutmvvm/TopFragment.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/TopFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import com.nemoramune.aboutmvvm.databinding.FragmentTopBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class TopFragment: Fragment(R.layout.fragment_top) {
@@ -22,6 +26,9 @@ class TopFragment: Fragment(R.layout.fragment_top) {
                 .load(imageUrl)
                 .into(imageView)
         }
+        viewModel.errorFlow.onEach {
+            Snackbar.make(view, "エラーしました！", Snackbar.LENGTH_SHORT).show()
+        }.launchIn(viewLifecycleOwner.lifecycleScope)
         viewModel.fetch()
 
         binding.button.setOnClickListener {
