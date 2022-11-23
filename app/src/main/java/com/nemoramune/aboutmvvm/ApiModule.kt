f622135e69860da6f46ec4057bbb1698c5aa4ae1
nemoramune <nemoramune@gmail.com>
2022-11-23 11:47:42 +0900

    ApiModuleをHiltモジュールに変更
    (cherry picked from commit 38f9d920606128f1ef1511348afb7e29a44fe242)

--- app/src/main/java/com/nemoramune/aboutmvvm/ApiModule.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/ApiModule.kt
@@ -2,19 +2,34 @@ package com.nemoramune.aboutmvvm
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object ApiModule {
 
-    private val moshi: Moshi = Moshi.Builder()
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi =  Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("https://foodish-api.herokuapp.com")
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .build()
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://foodish-api.herokuapp.com")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
 
-    val foodishApi = retrofit.create(FoodishApi::class.java)
+    @Singleton
+    @Provides
+    fun provideFoodishApi(retrofit: Retrofit):FoodishApi =
+        retrofit.create(FoodishApi::class.java)
 }
