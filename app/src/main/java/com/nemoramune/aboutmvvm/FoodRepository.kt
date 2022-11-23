ce99837bf0a065ed24f04eabde6f590fee40c687
nemoramune <nemoramune@gmail.com>
2022-11-23 11:50:19 +0900

    FoodishApiをFoodRepositoryで使用する
    (cherry picked from commit 28b7cb974115e7df0d672a2527ce0de6092958ca)

--- app/src/main/java/com/nemoramune/aboutmvvm/FoodRepository.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/FoodRepository.kt
@@ -5,5 +5,5 @@ import javax.inject.Inject
 class FoodRepository @Inject constructor(
     private val foodishApi: FoodishApi
 ) {
-    fun getRandomImageUrl() = "https://foodish-api.herokuapp.com/images/pizza/pizza50.jpg"
+    suspend fun getRandomImageUrl() = foodishApi.getRandomFood().image
 }
--- app/src/main/java/com/nemoramune/aboutmvvm/TopViewModel.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/TopViewModel.kt
@@ -3,7 +3,9 @@ package com.nemoramune.aboutmvvm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +13,8 @@ class TopViewModel @Inject constructor(private val foodRepository: FoodRepositor
     private val mutableImageUrlLiveData = MutableLiveData<String>()
     val imageUrlLiveData: LiveData<String> = mutableImageUrlLiveData
     fun fetch() {
-        mutableImageUrlLiveData.value = foodRepository.getRandomImageUrl()
+        viewModelScope.launch {
+            mutableImageUrlLiveData.value = foodRepository.getRandomImageUrl()
+        }
     }
 }
