636560b56e7d4eb42c4b80f6b0a832f514d1a98e
nemoramune <nemoramune@gmail.com>
2022-12-03 15:34:37 +0900

    PokemonRepositoryのgetRandomImageUrlがランダムになるように修正

--- app/src/main/java/com/nemoramune/aboutmvvm/PokemonRepository.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/PokemonRepository.kt
@@ -1,7 +1,11 @@
 package com.nemoramune.aboutmvvm
 
 import javax.inject.Inject
+import kotlin.random.Random
 
 class PokemonRepository @Inject constructor(private val pokemonApi: PokemonApi) {
-    suspend fun getRandomImageUrl() = pokemonApi.getPokemonData(1).sprites.frontDefault
+    suspend fun getRandomImageUrl(): String {
+        val randomId = Random.nextInt(1, 151)
+        return pokemonApi.getPokemonData(randomId).sprites.frontDefault
+    }
 }
