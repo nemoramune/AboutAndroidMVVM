e1f4b9068a3cf7c757fb7e183b1bc73b428ed01a
nemoramune <nemoramune@gmail.com>
2022-12-08 00:24:15 +0900

    HiltをつかってViewModelにRepositoryを注入

--- app/src/main/java/com/nemoramune/aboutmvvm/TopViewModel.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/TopViewModel.kt
@@ -4,13 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class TopViewModel: ViewModel() {
+class TopViewModel @Inject constructor(private val pokemonRepository: PokemonRepository): ViewModel() {
     private val mutableImageUrlLiveData = MutableLiveData<String>()
     val imageUrlLiveData: LiveData<String> = mutableImageUrlLiveData
-
-    private val pokemonRepository = PokemonRepository()
 
     fun fetch() {
         mutableImageUrlLiveData.value = pokemonRepository.getRandomImageUrl()
