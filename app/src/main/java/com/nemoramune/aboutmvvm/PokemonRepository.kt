c94d0ddfbb4c0f8ab4491cd2ab106467fc911efa
nemoramune <nemoramune@gmail.com>
2022-12-10 01:24:47 +0900

    PokemonRepositoryにPokemonApiを注入
    (cherry picked from commit 8c053d6bee24a129a0d93539c7fdc7dad52a178a)

--- app/src/main/java/com/nemoramune/aboutmvvm/PokemonRepository.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/PokemonRepository.kt
@@ -3,9 +3,9 @@ package com.nemoramune.aboutmvvm
 import javax.inject.Inject
 import kotlin.random.Random
 
-class PokemonRepository @Inject constructor() {
+class PokemonRepository @Inject constructor(private val pokemonApi: PokemonApi) {
     suspend fun getRandomImageUrl(): Result<String> = runCatching {
         val randomId = Random.nextInt(1, 151)
-        ApiClient.pokemonApi.getPokemonData(randomId).sprites.frontDefault
+        pokemonApi.getPokemonData(randomId).sprites.frontDefault
     }
 }
