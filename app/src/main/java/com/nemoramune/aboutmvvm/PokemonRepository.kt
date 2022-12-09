cc4596622c2a0f9777063ee8901a025f23c9d819
nemoramune <nemoramune@gmail.com>
2022-12-10 01:04:12 +0900

    PokemonApiをPokemonRepositoryで使用する

--- app/src/main/java/com/nemoramune/aboutmvvm/PokemonRepository.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/PokemonRepository.kt
@@ -1,5 +1,7 @@
 package com.nemoramune.aboutmvvm
 
 class PokemonRepository {
-    fun getRandomImageUrl() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+    suspend fun getRandomImageUrl(): String {
+        return ApiClient.pokemonApi.getPokemonData(1).sprites.frontDefault
+    }
 }
