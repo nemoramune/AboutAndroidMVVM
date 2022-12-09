6cd44e3ecab9484b66e80a310210777a85e42227
nemoramune <nemoramune@gmail.com>
2022-12-10 01:23:42 +0900

    ApiClientをApiModuleに変更

---
+++ app/src/main/java/com/nemoramune/aboutmvvm/ApiModule.kt
@@ -0,0 +1,35 @@
+package com.nemoramune.aboutmvvm
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi =  Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    @Singleton
+    @Provides
+    fun providePokemonApi(retrofit: Retrofit):PokemonApi =
+        retrofit.create(PokemonApi::class.java)
+}
