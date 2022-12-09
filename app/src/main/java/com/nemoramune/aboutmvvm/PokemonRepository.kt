9e79ebfde21d83672a132eff08235ad4c16dbbdf
nemoramune <nemoramune@gmail.com>
2022-12-10 01:22:16 +0900

    HiltをつかってViewModelにRepositoryを注入

--- app/src/main/java/com/nemoramune/aboutmvvm/PokemonRepository.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/PokemonRepository.kt
@@ -1,8 +1,9 @@
 package com.nemoramune.aboutmvvm
 
+import javax.inject.Inject
 import kotlin.random.Random
 
-class PokemonRepository {
+class PokemonRepository @Inject constructor() {
     suspend fun getRandomImageUrl(): Result<String> = runCatching {
         val randomId = Random.nextInt(1, 151)
         ApiClient.pokemonApi.getPokemonData(randomId).sprites.frontDefault
--- app/src/main/java/com/nemoramune/aboutmvvm/TopViewModel.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/TopViewModel.kt
@@ -8,16 +8,17 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class TopViewModel: ViewModel() {
+class TopViewModel @Inject constructor(
+    private val pokemonRepository: PokemonRepository
+): ViewModel() {
     private val mutableImageUrlLiveData = MutableLiveData<String>()
     val imageUrlLiveData: LiveData<String> = mutableImageUrlLiveData
 
     private val mutableErrorFlow = MutableSharedFlow<Throwable>()
     val errorFlow = mutableErrorFlow.asSharedFlow()
-
-    private val pokemonRepository = PokemonRepository()
 
     fun fetch() {
         viewModelScope.launch {
