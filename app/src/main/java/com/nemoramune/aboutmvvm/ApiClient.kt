5349bb07299a94508aa034b8921cf54889e7203a
nemoramune <nemoramune@gmail.com>
2022-12-14 00:32:34 +0900

    ApiClientのプロパティをProvidesに変更

--- app/src/main/java/com/nemoramune/aboutmvvm/ApiClient.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/ApiClient.kt
@@ -3,23 +3,33 @@ package com.nemoramune.aboutmvvm
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiClient {
 
-    private val moshi: Moshi = Moshi.Builder()
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi =  Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("https://pokeapi.co/api/v2/")
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .build()
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
 
-    val pokemonApi = retrofit.create(PokemonApi::class.java)
+    @Singleton
+    @Provides
+    fun providePokemonApi(retrofit: Retrofit):PokemonApi =
+        retrofit.create(PokemonApi::class.java)
 }
