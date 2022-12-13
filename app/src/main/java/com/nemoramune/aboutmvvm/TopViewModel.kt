bdbe01ff4fa56f4cb41e761d09b5c5b24f31d9e3
nemoramune <nemoramune@gmail.com>
2022-12-13 18:39:59 +0900

    TopViewModelにエラーハンドリングを追加

--- app/src/main/java/com/nemoramune/aboutmvvm/TopViewModel.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/TopViewModel.kt
@@ -4,11 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 class TopViewModel: ViewModel() {
     private val mutableImageUrlLiveData = MutableLiveData<String>()
     val imageUrlLiveData: LiveData<String> = mutableImageUrlLiveData
+
+    private val mutableErrorFlow = MutableSharedFlow<Throwable>()
+    val errorFlow = mutableErrorFlow.asSharedFlow()
 
     private val pokemonRepository = PokemonRepository()
 
@@ -16,6 +21,7 @@ class TopViewModel: ViewModel() {
         viewModelScope.launch {
              pokemonRepository.getRandomImageUrl()
                  .onSuccess { mutableImageUrlLiveData.value = it }
+                 .onFailure { error ->  mutableErrorFlow.emit(error) }
         }
     }
 }
