b8133c519da92363209aa26e42fc38a3d9596dc8
nemoramune <nemoramune@gmail.com>
2022-12-14 00:31:42 +0900

    ApiClientにHiltのアノテーションをつける

--- app/src/main/java/com/nemoramune/aboutmvvm/ApiClient.kt
+++ app/src/main/java/com/nemoramune/aboutmvvm/ApiClient.kt
@@ -2,9 +2,14 @@ package com.nemoramune.aboutmvvm
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
+@Module
+@InstallIn(SingletonComponent::class)
 object ApiClient {
 
     private val moshi: Moshi = Moshi.Builder()
